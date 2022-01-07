aa318f013451874c878eba2d42a49a3ad21f4220
Vanias de Paula <vanias_depaula@hotmail.com>
2022-01-06 21:51:35 -0400

    ajuste

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-})->middleware(['auth']);
+    return view('dashboard');
+})->middleware(['auth'])->name('dash');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
